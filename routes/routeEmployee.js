15e88e4521fcadbb33d89bb37b5936155e786291
julianjca <julianjca11@gmail.com>
2018-08-29 17:29:43 +0700

    front end 2.0

--- routes/routeEmployee.js
+++ routes/routeEmployee.js
@@ -1,15 +1,17 @@
-var router = require('express').Router()
+var express = require('express');
+var router = require('express').Router();
 var EmployeeController = require('../controllers/EmployeeController');
+router.use(express.static("public"));
 
 router.get('/', function(req, res) {
     res.render('employeeLogin');
-})
+});
 router.get('/register', function(req, res) {
     EmployeeController.register(req, res);
-})
+});
 
 
 
 
 
-module.exports = router
+module.exports = router;
